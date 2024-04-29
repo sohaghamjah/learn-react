6bc0b1f5bef0e7ca4e456ef5172fab3caab209c7
aymansohag <aymansohag.contact@gmail.com>
2024-04-29 23:34:54 +0600

    React props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css';
+import "./App.css";
 import Card from './components/Card';
 
 function App() {
   return (
     <div className='wrapper'>
       <h1 className='appHeading'>To Do App</h1>
-      <Card />
-      <Card />
-      <Card />
+      <Card cardTitle="React With Next Js" cardDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "/>
+      <Card cardTitle="Vue With Nuxt Js" cardDesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "/>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,11 @@
-const cardTitle = "Hello React Card";
-const cardDesc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ";
-
-function Card(){
+function Card(props){
+  const {cardTitle, cardDesc} = props;
   return (
     <div className='card'>
-        <h3 className='cardTitle'>{cardTitle}</h3>
-        <p className='cardDesc'>{cardDesc}</p>
-        <p className='cardFooter'>Sohag Hamjah</p>
-      </div>
+      <h3 className='cardTitle'>{cardTitle}</h3>
+      <p className='cardDesc'>{cardDesc}</p>
+      <p className='cardFooter'>Sohag Hamjah</p>
+    </div>
   )
 }
 
