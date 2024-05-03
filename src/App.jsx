deefd7864d93818fde8c043724a8ccaf5faa9060
aymansohag <aymansohag.contact@gmail.com>
2024-05-03 23:05:53 +0600

    functional component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Card from './components/Card';
 import Data from './data.json';
 import { v4 as uuidv4 } from 'uuid';
+import Card2 from "./components/Card2";
 
 let items = [];
     items = Data.map((item) => <Card key={uuidv4()} cardTitle={item.title} cardDesc={item.desc} /> );
@@ -12,6 +13,7 @@ function App() {
     <div className='wrapper'>
       <h1 className='appHeading'>To Do App</h1>
       {items}
+      <Card2 CardTitle="Hello Class Component" />
     </div>
   )
 }
