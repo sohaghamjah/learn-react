29da46c4f658754a81d64685a720fcbd5013a398
aymansohag <aymansohag.contact@gmail.com>
2024-05-03 23:39:25 +0600

    bootstrap setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
-import Card from './components/Card';
-import Data from './data.json';
-import { v4 as uuidv4 } from 'uuid';
-import Card2 from "./components/Card2";
-
-let items = [];
-    items = Data.map((item) => <Card key={uuidv4()} cardTitle={item.title} cardDesc={item.desc} /> );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactBootstrap from "./components/React_Bootstrap/ReactBootstrap";
 
 
 function App() {
   return (
     <div className='wrapper'>
       <h1 className='appHeading'>To Do App</h1>
-      {items}
-      <Card2 CardTitle="Hello Class Component" />
+      <ReactBootstrap />
     </div>
   )
 }
---
+++ src/components/React_Bootstrap/ReactBootstrap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function ReactBootstrap() {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default ReactBootstrap
